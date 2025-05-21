0c32213cf77e6d8c6726b1d106c0e81eb10c2767
brandy-build <chawda7prajwal@gmail.com>
2025-05-21 07:31:26 +0530

    This is MAD repo
    .

---
+++ MyCameraApp/MyCameraApp/app/src/main/java/com/example/mycameraapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.mycameraapp
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private val CAMERA_PERMISSION_CODE = 100
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Camera permission already granted", Toast.LENGTH_SHORT).show()
+            // Here you can access camera
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                Toast.makeText(this, "Camera permission granted", Toast.LENGTH_SHORT).show()
+                // Here you can access camera
+            } else {
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
